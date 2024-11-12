307d8a1be4a0fc570989c6a53d7e4c69d9619f2e
theaminuldev <mdaminulislam2015bd@gmail.com>
2024-11-12 22:17:02 +0600

    Update .gitignore, composer.json, and add initial Exoole plugin files

--- exoole.php
+++ exoole.php
@@ -6,14 +6,27 @@
  * Plugin URI: https://wordpress.org/plugins/exoole
  * Description: Exoole is a plugin that helps you to create an any design.
  * Version: 1.0.0
- * Author: theaminul
+ * Author: theaminul,
  * Author URI: https://github.com/theaminuli
  * License: GNU General Public License v3 or later
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: exoole
+ * 
+ *  @package CreateBlock
  */
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+ if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function create_block_example_dynamic_block_init() {
+	register_block_type( __DIR__ . '/build' );
+}
+add_action( 'init', 'create_block_example_dynamic_block_init' );
