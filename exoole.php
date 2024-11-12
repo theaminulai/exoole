e4f3f6effbb90195741642ec283c9ed2e9c4d527
theaminuldev <mdaminulislam2015bd@gmail.com>
2024-11-12 07:33:06 +0600

    Add Exoole plugin with initial setup and silence index file

---
+++ exoole.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name: Exoole
+ * Plugin URI: https://wordpress.org/plugins/exoole
+ * Description: Exoole is a plugin that helps you to create an any design.
+ * Version: 1.0.0
+ * Author: Aminul Islam
+ * Author URI: https://github.com/theaminuli
+ * License: GNU General Public License v3 or later
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: exoole
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
