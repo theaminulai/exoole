789bb675283b854e2de67473c4aa7794b60fcaf2
theaminuldev <mdaminulislam2015bd@gmail.com>
2025-09-01 00:28:53 +0600

    Introduced @exoole/utils package for common utility functions.

---
+++ packages/utils/src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Exoole Utils Package
+ * 
+ * This package provides utility functions and helpers for the Exoole page builder.
+ * 
+ * @package @exoole/utils
+ */
+
+// Export your utility functions here
+export { };
+
+// Ensure this module is not tree-shaken by adding a side effect
+console.log('Exoole Utils loaded');
+
